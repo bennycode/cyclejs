362640d7543338ce97259f040cb80446b29f9e5b
Jan van Brügge <supermanitu@gmail.com>
2018-07-20 14:09:19 +0200

    refactor(isolate): move isolate tooling to pnpm

--- isolate/test/index.ts
+++ isolate/test/index.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import * as assert from 'assert';
 import * as Rx from 'rxjs';
-import isolate from '../lib/cjs/index';
+import isolate from '../src/index';
 import * as sinon from 'sinon';
 
 describe('isolate', function() {
